5c5f74a66b3298b6da193c821c1dba5c35852422
jmsmith1018 <jmsmith1018@gmail.com>
2022-11-30 23:32:25 -0700

    cleanup for day1 solution

--- day01/puzzle.rb
+++ day01/puzzle.rb
@@ -1,9 +1,7 @@
 # frozen_string_literal: true
 
 input = File.read(*ARGV).split("\n\n")
-group_totals = input.reduce([]) do |totals, elf_snacks|
-  totals << elf_snacks.split.sum(&:to_i)
-end
+group_totals = input.map { |snack_group| snack_group.split.sum(&:to_i) }
 
 p group_totals.max # p1-> 68802
 
