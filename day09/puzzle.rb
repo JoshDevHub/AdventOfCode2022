25ec4717234abef302d115772b6dd2c98bc44add
jmsmith1018 <jmsmith1018@gmail.com>
2022-12-09 00:37:49 -0700

    add solution to day9 in ruby

---
+++ day09/puzzle.rb
@@ -0,0 +1,44 @@
+# frozen_string_literal: true
+
+require "set"
+
+@input = File.readlines(*ARGV, chomp: true).map(&:split)
+
+Point = Struct.new(:x, :y) do
+  def move_in(dir)
+    case dir
+    when "L" then self.x -= 1
+    when "R" then self.x += 1
+    when "D" then self.y -= 1
+    when "U" then self.y += 1
+    end
+  end
+
+  def keep_adjacency_with(other)
+    dx = other.x - x
+    dy = other.y - y
+    return if dx.abs < 2 && dy.abs < 2
+
+    self.x += dx.zero? ? 0 : dx / dx.abs
+    self.y += dy.zero? ? 0 : dy / dy.abs
+  end
+end
+
+def count_uniq_tail_positions(knot)
+  visit_set = Set.new
+  @input.each do |dir, count|
+    count.to_i.times do
+      head = knot[0]
+      head.move_in(dir)
+      knot.each_cons(2) { |prev, curr| curr.keep_adjacency_with(prev) }
+      visit_set << knot[-1].clone
+    end
+  end
+  visit_set.size
+end
+
+short_knot = Array.new(2) { Point.new(0, 0) }
+p count_uniq_tail_positions(short_knot)
+
+long_knot = Array.new(10) { Point.new(0, 0) }
+p count_uniq_tail_positions(long_knot)
